705f73700b4fe33b79e366bc6ef3ce2972d34b4a
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2024-03-10 19:28:40 -0400

    finish video 5

--- Scene1.js
+++ Scene1.js
@@ -5,9 +5,22 @@ class Scene1 extends Phaser.Scene{
 
     preload(){
         this.preload.image("background", "assets/images/background.png");
-        this.preload.image("ship", "assets/images/ship.png");
-        this.preload.image("ship2", "assets/images/ship2.png");
-        this.preload.image("ship3", "assets/images/ship3.png");
+        this.preload.spritesheet("ship", "assets/spritesheets/ship.png",{
+            frameWidth: 16,
+            frameHeight:16,
+        });
+        this.preload.spritesheet("ship2", "assets/spritesheets/ship2.png",{
+            frameWidth: 32,
+            frameHeight:16,
+        });
+        this.preload.spritesheet("ship3", "assets/spritesheets/ship3.png",{
+            frameWidth: 32,
+            frameHeight:32,
+        });
+        this.preload.spritesheet("explosion", "assets/spritesheets/explosion.png",{
+            frameWidth: 16,
+            frameHeight:16,
+        });
     }
 
     create() {
