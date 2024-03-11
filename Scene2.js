3c4e65f923287082919911d4f722cc4c3e5578e9
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2024-03-10 21:35:49 -0400

    Music added

--- Scene2.js
+++ Scene2.js
@@ -76,6 +76,24 @@ class Scene2 extends Phaser.Scene{
 
         this.score = 0;
         this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE", 16);
+
+        this.beamSound = this.sound.add("audio_beam");
+        this.explosionSound = this.sound.add("audio_explosion");
+        this.pickupSound = this.sound.add("audio_pickup");
+
+        this.music.this.sound.add("music");
+
+        var musicConfig = {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: false,
+            delay: 0
+        }
+
+        this.music.play(musicConfig);
     }
 
     zeroPad(number,size){
@@ -146,6 +164,7 @@ class Scene2 extends Phaser.Scene{
 
     pickPowerUp(player, powerUp) {
         powerUp.disableBody(true, true);
+        this.pickupSound.play();
     }
 
     hurtPlayer(player, enemy) {
@@ -171,10 +190,13 @@ class Scene2 extends Phaser.Scene{
         this.score += 15;
         var scoreFormated = this.zeroPad(this.score, 6);
         this.scoreLabel.text = "SCORE" + scoreFormated;
+
+        this.explosionSound.play();
     }
 
     shootBeam(){
         var beam = new Beam(this);
+        this.beamSound.play();
     }
 
     resetPlayer(){
